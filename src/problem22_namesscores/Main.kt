d887a03cce5f592eaee32642757a0fc24d6d3416
Taras Lavreniuk <taras@staffbase.com>
2020-04-10 15:47:54 +0200

    Add: problem 22 Names scores https://projecteuler.net/problem=22

---
+++ src/problem22_namesscores/Main.kt
@@ -0,0 +1,35 @@
+package problem22_namesscores
+
+import java.io.File
+
+/**
+ * Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first
+ * names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply
+ * this value by its alphabetical position in the list to obtain a name score.
+ *
+ * For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is
+ * the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+ *
+ * What is the total of all the name scores in the file?
+ */
+object Main {
+
+  @JvmStatic
+  fun main(args: Array<String>) {
+    var result = 0
+    readListOfNames().sorted().forEachIndexed { index, name ->
+      result += (index + 1) * calculateNameWeight(name)
+    }
+    println(result)
+  }
+
+  private fun readListOfNames() = File("src/problem22_namesscores/names.txt").readText().replace("\"", "").split(",")
+
+  private fun calculateNameWeight(name: String): Int {
+    var result = 0
+    name.forEach {
+      result += it.toInt() - 64
+    }
+    return result
+  }
+}
